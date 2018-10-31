63cbb6308e595c5c06630858960404f498f10073
Haggis Breakfast <lj.stewart00@gmail.com>
2018-10-31 14:32:46 -0700

    connected app to websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,10 @@ class App extends Component {
         },
       ],
     };
+    // create a websocket connection to our server
+
+    this.socket = new WebSocket('ws://localhost:3001');
+
     this.addMessage = this.addMessage.bind(this);
   }
 
@@ -38,16 +42,20 @@ class App extends Component {
   }
 
   componentDidMount() {
+    this.socket.onopen = () => {
+      console.log('Connected to Server');
+    };
+
     console.log('componentDidMount <App />');
-    setTimeout(() => {
-      console.log('Simulating incoming message');
-      // Add a new message to the list of messages in the data store
-      const newMessage = { id: this.state.messages.length, username: 'Michelle', content: 'Hello there!' };
-      const messages = this.state.messages.concat(newMessage);
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({ messages: messages });
-    }, 3000);
+    // setTimeout(() => {
+    //   console.log('Simulating incoming message');
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = { id: this.state.messages.length, username: 'Michelle', content: 'Hello there!' };
+    //   const messages = this.state.messages.concat(newMessage);
+    //   // Update the state of the app component.
+    //   // Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({ messages: messages });
+    // }, 3000);
   }
 
   render() {
